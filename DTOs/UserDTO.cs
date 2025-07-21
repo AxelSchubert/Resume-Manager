6dc8508298dcf0f2b65623208b2c92bbab2f0e3a
AxelSchubert <axel.schubert@chasacademy.se>
2025-07-21 15:49:26 +0200

    added_userservices

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -5,6 +5,8 @@
         public string FullName { get; set; }
         public string Description { get; set; }
         public string Email { get; set; }
+        public List<EducationDTO> Educations { get; set; }
+        public List<WorkExperienceDTO> WorkExperiences { get; set; }
 
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,67 @@
-﻿namespace Resume_Manager.Services
+﻿using Resume_Manager.Models;
+using Resume_Manager.Data;
+using Resume_Manager.DTOs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace Resume_Manager.Services
 {
     public class UserService
     {
+        public async Task<List<UserDTO>> FetchAllUsers(ManagerDbContext context)
+        {
+            var users = await context.Users.Select(u => new UserDTO
+            {
+                FullName = u.FullName,
+                Description = u.Description,
+                Email = u.Email,
+                Educations = u.Educations.Select(e => new EducationDTO
+                {
+                    SchoolName = e.SchoolName,
+                    Degree = e.Degree,
+                    StartDate = e.StartDate,
+                    EndDate = e.EndDate
+                }
+                ).ToList(),
+                WorkExperiences = u.WorkExperiences.Select(w => new WorkExperienceDTO
+                {
+                    CompanyName = w.CompanyName,
+                    JobTitle = w.JobTitle,
+                    Description = w.Description,
+                    StartDate = w.StartDate,
+                    EndDate = w.EndDate
+                }).ToList()
+            }).ToListAsync();
+
+            return users;
+        }
+
+        public async Task<UserDTO> FetchUserByID(ManagerDbContext context, int userId)
+        {
+            var user = await context.Users.Where(u => u.UserId == userId).Select(u => new UserDTO
+            {
+                FullName = u.FullName,
+                Description = u.Description,
+                Email = u.Email,
+                Educations = u.Educations.Select(e => new EducationDTO
+                {
+                    SchoolName = e.SchoolName,
+                    Degree = e.Degree,
+                    StartDate = e.StartDate,
+                    EndDate = e.EndDate
+                }
+                ).ToList(),
+                WorkExperiences = u.WorkExperiences.Select(w => new WorkExperienceDTO
+                {
+                    CompanyName = w.CompanyName,
+                    JobTitle = w.JobTitle,
+                    Description = w.Description,
+                    StartDate = w.StartDate,
+                    EndDate = w.EndDate
+                }).ToList()
+            }).FirstOrDefaultAsync();
+            
+            return user;
+        }
     }
 }
